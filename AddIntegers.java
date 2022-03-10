4d2226d35e3bb52a576cdcfc66c1af05ae165145
cbourn314159 <71228196+cbourn314159@users.noreply.github.com>
2022-03-10 14:31:22 -0500

    Added correct code and changed integer values

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,1 +1,9 @@
-//added comment to CHRISTOPHER BOURN branch
+public class AddIntegers {
+    public static void main(String[] args) {
+        int first = 30;
+        int second = 40;
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+        System.out.println("The sum is: " + sum);
+    }
+}
